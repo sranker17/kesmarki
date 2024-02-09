4b22a4663442d9d3119e13ab23573219bc84acc2
sranker17 <vrabeldani@gmail.com>
2024-02-09 20:09:36 +0100

    add getter and setter to ErrorMessage class

--- src/main/java/com/kesmarki/task/exception/ErrorMessage.java
+++ src/main/java/com/kesmarki/task/exception/ErrorMessage.java
@@ -1,8 +1,12 @@
 package com.kesmarki.task.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor
+@Getter
+@Setter
 public class ErrorMessage {
     String message;
 }
